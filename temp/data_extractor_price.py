c30581791bb4a42f2b5d5bc669dcd9c92ecbb6e6
Lere16 <lere.deguenon2@gmail.com>
2024-10-12 06:25:54 +0100

    update data
    Signed-off-by: Lere16 <lere.deguenon2@gmail.com>

--- temp/data_extractor_price.py
+++ temp/data_extractor_price.py
@@ -2,23 +2,23 @@ import pandas as pd
 
 # Load the CSV file
 year=2023
-file_path = 'loads/load2023.csv'   # Replace with the path to your CSV file
+file_path = 'prices/price2023.csv'   # Replace with the path to your CSV file
 df = pd.read_csv(file_path)
 
 # Convert the 'Time' column to datetime and create a new 'Start_Time' column
-df['Time'] = pd.to_datetime(df['Time (CET/CEST)'].str.split(' - ').str[0], format='%d.%m.%Y %H:%M')
+df['Time'] = pd.to_datetime(df['MTU (CET/CEST)'].str.split(' - ').str[0], format='%d.%m.%Y %H:%M')
 
 # Filter for rows where the minute is 00 (i.e., on the hour)
 hourly_data = df[df['Time'].dt.minute == 0]
 
 # Drop unnecessary columns
-hourly_data = hourly_data[['Time', 'Day-ahead Total Load Forecast [MW]', 'Actual Total Load [MW]']]
+hourly_data = hourly_data[['Time', 'Day-ahead Price [EUR/MWh]', 'Currency']]
 
 # Rename columns for clarity
-hourly_data.columns = ['Time', 'Day-ahead Total Load Forecast [MW]', 'Actual Total Load [MW]']
+hourly_data.columns = ['Time', 'Day-ahead Price [EUR/MWh]', 'Currency']
 
 # Save the result to a new CSV file
-hourly_data.to_csv(f"hourly_load_{year}.csv", index=False)
+hourly_data.to_csv(f"hourly_DA_price_{year}.csv", index=False)
 
 # Display the first few rows of the filtered data
 print(hourly_data.head())
